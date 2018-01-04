e38877e5262b4c94941dd8977eba931997d2256b
mauyeung <mauyeung74@gmail.com>
2018-01-04 01:39:04 -0500

    initial setup

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+var webpack = require('webpack');
+var path = require('path');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+
+module.exports = {
+  context: path.join(__dirname, "src"),
+  entry: "./index.js",
+  watch: true,
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'env']
+        }
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'url-loader?limit=8192'
+      }
+    ]
+  },
+  plugins: [
+    new UglifyJsPlugin()
+  ],
+  output: {
+    path: __dirname + "/build/",
+    filename: "browser.min.js"
+  }
+};
